6d20a5e026255eb82ac7343885586e14b22e9409
joeypy <joeypc.py@gmail.com>
2020-04-11 14:37:17 -0400

    Componente de mapas con leaflet y OpenStreetMap

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,51 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [50, 82],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent {
+
+  private map;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    L.marker([ 10.4745864, -66.5381958 ]).addTo(this.map);
+  }
+
+  private initMap(): void {
+
+    this.map = L.map('map', {
+      center: [ 10.4745864, -66.5381958 ],
+      zoom: 18
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+  
+
+}
